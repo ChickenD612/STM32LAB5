16e06bc2162887f69ca91b68492895fffaff1286
DUNGODANGO\ASUS <duong.nguyen060102@hcmut.edu.vn>
2023-12-04 21:54:49 +0700

    main

---
+++ SOURCE_CODE/LAB4/Core/Inc/command_parser_fsm.h
@@ -0,0 +1,19 @@
+/*
+ * command_parser_fsm.h
+ *
+ *  Created on: Nov 19, 2023
+ *      Author: ASUS
+ */
+
+#ifndef INC_COMMAND_PARSER_FSM_H_
+#define INC_COMMAND_PARSER_FSM_H_
+
+#include "global.h"
+
+extern uint8_t command_flag;
+extern uint8_t command_data[10];
+extern uint8_t length_of_command;
+
+void command_parser_fsm();
+
+#endif /* INC_COMMAND_PARSER_FSM_H_ */
